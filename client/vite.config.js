7a8fec00f6175e49762c128d891a5fc7297862b6
Kaveri-Bhimappa-Jarali <kaverijarali22@gmail.com>
2025-10-30 07:53:32 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";  // if using React
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-})
+  plugins: [ react(), tailwindcss() ],
+});
